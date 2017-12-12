bec4aabf2ede96e333157f31f25ae10387e760db
Who Soup <who.soup@gmail.com>
2017-12-12 07:37:40 +0100

    MUCH better solution

--- day12.js
+++ day12.js
@@ -1,35 +1,15 @@
 const fs = require('fs')
-let inp = fs.readFileSync("./day12input").toString('utf-8').trim().split(/[\r\n]+/)
-  .map((x) => x.split(" <-> ")[1].split(", ").map(y => parseInt(y)))
+let inp = fs.readFileSync("./day12input").toString('utf-8').trim().split(/[\r\n]+/) // array of lines
+  .map((x) => x.split(">")[1].split(", ").map(y => +y))
 
-
-let nodes = []
-function Node() {
-  this.visited = 0
-  this.neighbors = []
-  this.reach = function() {
-    if (this.visited++) return 0
-    let sum = 0
-    for (let i of this.neighbors)
-      sum += nodes[i].reach()
-    return 1 + sum
-  }
+let visited = []
+function reach(i) {
+  if (visited.includes(i))
+    return 0
+  visited.push(i)
+  return inp[i].reduce((a,b) => a + reach(b), 1)
 }
 
-for (let i of inp) {
-  let n = new Node()
-  n.neighbors = i
-  nodes.push(n)
-}
-
-let groups = 0
-for (let i in inp) {
-  let reach = nodes[i].reach()
-  if (reach > 0) {
-    groups++
-  }
-  if (i == 0)
-    console.log(reach);
-}
-
-console.log(groups);
+inp = inp.map((_, k) => reach(k))
+console.log("a:", inp[0]);
+console.log("b:", inp.filter(x => x > 0).length);

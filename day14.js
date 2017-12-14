d200bd2aa14de1269b5d9095ed9ce061da1cce3b
Who Soup <who.soup@gmail.com>
2017-12-14 06:52:06 +0100

    quick and dirty

---
+++ day14.js
@@ -0,0 +1,64 @@
+let input = "xlqgujun", grid = [], used = 0
+for (let i = 0; i < 128; i++) {
+  let h = hash(input + "-" + i)
+  let b = h.split("").map(x => ("0000"+ parseInt(x, 16).toString(2)).substr(-4)).join("") // convert hash to binary
+  used += b.split("").filter(x => x == 1).length // count 1s
+  grid.push(b.split("")) // add to grid
+}
+console.log(used);
+
+let c = (x,y) => (x < 0 || y < 0 || x > 127 || y > 127) ? 0 : grid[x][y]
+
+function removeGroup(x,y) {
+  if (c(x, y) == 0) return
+  grid[x][y] = 0
+  removeGroup(x + 1, y)
+  removeGroup(x - 1, y)
+  removeGroup(x, y + 1)
+  removeGroup(x, y - 1)
+}
+
+let groups = 0
+for (let x = 0; x < 128; x++)
+  for (let y = 0; y < 128; y++)
+    if (c(x,y) == 1) {
+      groups++
+      removeGroup(x, y)
+    }
+console.log(groups);
+
+//
+// code below from day 10
+//
+function hash(inx) {
+  if (!Array.prototype.reverseSection) {
+    Array.prototype.reverseSection = function (start, len) {
+      for (let i = 0; i < len/2; i++) {
+        let a = (start + i) % this.length,
+            b = (start + len - i - 1) % this.length; // semicolon needed
+        [this[a], this[b]] = [this[b], this[a]]
+      }
+    }
+  }
+
+  let inp = inx.split("").map(x => x.charCodeAt(0)).concat([17, 31, 73, 47, 23])
+  let n = 256
+  let cycles = 64
+  let list = [], pos = 0, skip = 0
+  while (--n != -1)
+    list.unshift(n)
+
+  // calculate hash
+  while (cycles--)
+    for (let length of inp) {
+      list.reverseSection(pos, length)
+      pos = (pos + length + skip++) % list.length
+    }
+
+    let dense = []
+    for (let i = 0; i < 256; i += 16)
+      dense.push(list.slice(i, i + 16).reduce((a, b) => a^b))
+
+      // ugly hack because javascript doesn't do leading zeroes
+      return dense.map(x => ("0" + x.toString(16)).substr(-2)).join("")
+}

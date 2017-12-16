3a42d602ae047bc849ed9bba220e24562f68ee5d
Who Soup <who.soup@gmail.com>
2017-12-16 08:03:03 +0100

    cleanup

--- day16.js
+++ day16.js
@@ -1,31 +1,20 @@
 const fs = require('fs')
 let inp = fs.readFileSync("./day16input").toString('utf-8').trim().split(",")
 
-const exchange = (a,b) => [dancer[a], dancer[b]] = [dancer[b], dancer[a]]
-const partner = (a,b) => exchange(dancer.indexOf(a), dancer.indexOf(b))
-const spin = (x) => dancer = [...dancer.slice(-x), ...dancer.slice(0,-x)]
+let fn = {
+  'x': (a,b) => [dancer[a], dancer[b]] = [dancer[b], dancer[a]],
+  'p': (a,b) => fn.x(dancer.indexOf(a), dancer.indexOf(b)),
+  's': (x) => dancer = [...dancer.slice(-x), ...dancer.slice(0,-x)]
+}
+let dance = () => inp.forEach(x => fn[x.charAt(0)](...x.substr(1).split('/')))
 
 let dancer = "abcdefghijklmnop".split("")
 let perms = []
 
-function dance() {
-    inp.forEach(x => {
-      if (x.charAt(0) == 'x')
-        exchange(...x.substr(1).split('/').map(x => +x))
-      else if (x.charAt(0) == 's')
-        spin(+x.substr(1))
-      else if (x.charAt(0) == 'p')
-        partner(...x.substr(1).split('/'))
-  })
-  perms.push(dancer)
-}
-
-// part one
-dance()
-console.log(dancer.join(""));
-
-// part two
-while (dancer.join("") != "abcdefghijklmnop")
+do {
+  perms.push(dancer.join(""))
   dance()
+} while (dancer.join("") != "abcdefghijklmnop")
 
-console.log(perms[1000000000 % perms.length - 1].join(""));
+console.log(perms[1]);
+console.log(perms[1000000000 % perms.length]);
